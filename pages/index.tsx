42196957946d06be8d24d524f6badfa7f16de7f1
luke-h1 <luke.howsam@yahoo.com>
2021-02-06 17:37:47 +0000

    adjust husky

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,9 +7,7 @@ import Layout from '../components/Layout/Layout';
 export default function Home({ data }) {
   const [search, setSearch] = useState('');
 
-  const filterCoins = data.filter((coin) =>
-    coin.name.toLowerCase().includes(search.toLowerCase())
-  );
+  const filterCoins = data.filter((coin) => coin.name.toLowerCase().includes(search.toLowerCase()));
 
   const handleChange = (e) => {
     e.preventDefault();
@@ -34,8 +32,7 @@ export default function Home({ data }) {
 }
 
 export const getServerSideProps = async () => {
-  const API_URL =
-    'https://api.coingecko.com/api/v3/coins/markets?vs_currency=gbp&order=market_cap_desc&per_page=30&page=1&sparkline=false';
+  const API_URL = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=gbp&order=market_cap_desc&per_page=30&page=1&sparkline=false';
   const res = await fetch(API_URL);
   const data = await res.json();
   return {
